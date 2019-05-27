6ccaeaf2e23d1dcfc1c6b981a75c87a34ab9cddc
Saroj Kunwar <rawnuk01@gmail.com>
2019-05-27 16:14:22 -0400

    Designing UICollectionView Cells.

---
+++ 10NameToFaces/PersonViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  PersonViewCell.swift
+//  10NameToFaces
+//
+//  Created by Saroj on 5/27/19.
+//  Copyright © 2019 Saroj. All rights reserved.
+//
+
+import UIKit
+
+class PersonViewCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+}
