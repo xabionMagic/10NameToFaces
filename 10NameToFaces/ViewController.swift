9e2d4564df2679df5fd512ccfc8dbec689d057eb
Saroj Kunwar <rawnuk01@gmail.com>
2019-05-27 16:34:35 -0400

    UICollectionVIew Data Sources

--- 10NameToFaces/ViewController.swift
+++ 10NameToFaces/ViewController.swift
@@ -16,5 +16,16 @@ class ViewController: UICollectionViewController {
     }
 
 
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonViewCell else {
+            fatalError("Unable to dequeue PersonCell.")
+        }
+        
+        return cell
+    }
 }
 
