a9921e3c770b4e40f5dadb20ddf6e426866633ff
Saroj Kunwar <rawnuk01@gmail.com>
2019-05-27 17:26:59 -0400

    Importing Photos with UIImagePickerController

--- 10NameToFaces/ViewController.swift
+++ 10NameToFaces/ViewController.swift
@@ -8,24 +8,64 @@
 
 import UIKit
 
-class ViewController: UICollectionViewController {
+class ViewController: UICollectionViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewPerson))
         // Do any additional setup after loading the view.
     }
 
+    @objc func addNewPerson() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
 
+    //MARK:- UICollective Delegate
+    
+    //--->> numberofItems in Sections
+    
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 10
     }
     
+    // ---->>> cellForItemAt
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonViewCell else {
             fatalError("Unable to dequeue PersonCell.")
         }
         
         return cell
+    }
+    
+    //MARK:- ImagePicker Delegate
+    
+    //----->>>> DidFinishPickingMediaWithInfo
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage  else {
+            return
+        }
+        
+        let imageName = UUID().uuidString
+        let imagePath = getDocumentsDirectory().appendingPathComponent(imageName)
+        
+        //convert to jpegData
+        if let jpegData = image.jpegData(compressionQuality: 0.8) {
+            try? jpegData.write(to: imagePath)
+        }
+        
+        //dismiss the topmost viewController.
+        dismiss(animated: true)
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        
+        return paths[0]
     }
 }
 
